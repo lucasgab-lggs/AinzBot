2cb18398b15c1d77f1551424d96eab98e763c23d
Lucas Gabriel <lucasgab.lggs@gmail.com>
2024-09-03 16:16:13 -0300

    style(events): update output messages for ready event

--- src/events/main/ready.ts
+++ src/events/main/ready.ts
@@ -6,7 +6,7 @@ export default new Event({
     once: true,
     run() {
         const { commands, buttons, selects, modals } = client
-        console.log('✅ Bot online'.green);
+        console.log('✅ Bot online!'.green);
         console.log(`Commands loaded: ${commands.size}`.cyan);
         console.log(`Buttons loaded: ${buttons.size}`.cyan);
         console.log(`Selects loaded: ${selects.size}`.cyan);
